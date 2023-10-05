eaf196dbac494406336d4c8f5abd844efd3586c0
David <davite3@gmail.com>
2023-10-05 11:46:54 +0100

    problem 3

---
+++ lab1/problem-3/Content.js
@@ -0,0 +1,48 @@
+// replace every lowercase L with a capital I
+// and every capital I with alowercase L
+// add funny images to let the user know they have been pranked!
+var main = function () {
+    var scheming = [
+        "https://st4.depositphotos.com/1001911/21888/v/1600/depositphotos_218880448-stock-illustration-scheming-emoticon.jpg",
+        "https://i.kym-cdn.com/entries/icons/original/000/031/260/Screen_Shot_2019-09-24_at_4.22.16_PM.png",
+        "https://media.tenor.com/IQcYOoTEuXYAAAAC/holy-moly.gif",
+        "https://media.tenor.com/PJCNWkE311YAAAAd/slowly-turning-emoji.gif"
+    ];
+    var imgs = document.getElementsByTagName("img");
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].src = scheming[Math.floor(Math.random() * scheming.length)];
+    }
+    var p = document.getElementsByTagName("p");
+    var lis = document.getElementsByTagName("li");
+    var prank = function (ps) {
+        for (var i = 0; i < ps.length; i++) {
+            var par = ps[i].innerText.split('');
+            for (var ii = 0; ii < par.length; ii++) {
+                if (par[ii] === "l")
+                    par[ii] = "I";
+                else if (par[ii] === "I")
+                    par[ii] = "l";
+            }
+            ps[i].innerText = par.join('');
+            ps[i].style.color = ("#" + Math.floor(Math.random() * 255).toString(16) +
+                Math.floor(Math.random() * 255).toString(16) +
+                Math.floor(Math.random() * 255).toString(16));
+        }
+    };
+    prank(p);
+    prank(lis);
+    var h1s = document.getElementsByTagName("h1");
+    prank(h1s);
+    var goof = function (hs) {
+        for (var i = 0; i < hs.length; i++) {
+            hs[i].innerText = "you just got pranked!";
+        }
+    };
+    goof(h1s);
+    goof(document.getElementsByTagName("h2"));
+    goof(document.getElementsByTagName("h3"));
+    goof(document.getElementsByTagName("h4"));
+    goof(document.getElementsByTagName("h5"));
+    goof(document.getElementsByTagName("h6"));
+};
+main();
--- lab1/problem-3/README.md
+++ lab1/problem-3/README.md
@@ -1,1 +1,16 @@
-# Hello world
+# The most annoying extension I can think of
+
+This extension is designed to be as annoying and inconvenient as possible
+Features include: 
+- Replacing all capital 'I' with lowercase 'l' and vice versa
+- Randomizing the colour of every paragraph, list element and header
+- Funny image replacements.
+- Destroys most hyperlinks
+
+Sites to try:
+
+- github.com
+- wikipedia.org
+
+I found this extension to be progressively more infuriating as I was writing it and looking up docs in the process.
+
