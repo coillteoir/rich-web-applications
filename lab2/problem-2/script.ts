c35378f8e9ce3a05204adab51ec9790cf066efef
David <davite3@gmail.com>
2023-10-30 18:23:41 +0000

    problem 2 complete

---
+++ lab2/problem-2/script.ts
@@ -0,0 +1,27 @@
+
+//Q1
+fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then(response => response.json())
+    .then(json => console.log(
+        json.map(obj => obj.title).filter(str => str.split(' ').length >= 6)
+    ))
+//Q2
+
+fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then(response => response.json())
+    .then(json => console.log(
+        json.map(obj => obj.body)
+        .reduce((acc, curr) => acc + curr)
+        .split(' ')
+        .map(e => e.split('\n'))
+        .flat()
+        .sort()
+        .reduce((acc, curr) => {
+            if(!acc[curr]){
+                acc[curr] = 1
+            } else {
+                acc[curr]++;
+            }
+            return acc
+        }, {})
+    ))
