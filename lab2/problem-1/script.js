4bef64ce2368c1bede1e5bad10bc4e41f18274e1
David <davite3@gmail.com>
2023-10-31 12:49:43 +0000

    problem 1

--- lab2/problem-1/script.js
+++ lab2/problem-1/script.js
@@ -26,13 +26,29 @@ var generateContact = function (form) {
     var name = inputData.find(function (x) { return x.name == "fName"; }).value;
     var number = inputData.find(function (x) { return x.name == "number"; }).value;
     var email = inputData.find(function (x) { return x.name == "email"; }).value;
+    inputData.find(function (x) { return x.name == "fName"; }).value = "";
+    inputData.find(function (x) { return x.name == "number"; }).value = "";
+    inputData.find(function (x) { return x.name == "email"; }).value = "";
     var emailPattern = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
     var phoneNumberPattern = /^\d{10}$/;
-    console.log(phoneNumberPattern.test(number), emailPattern.test(email));
     if (name === "" || !phoneNumberPattern.test(number) || !emailPattern.test(email)) {
         alert("Invalid input");
     }
     else {
         addToTable(new Contact(name, number, email));
     }
+};
+var searchNumber = function (value) {
+    var table = document.getElementById("contacts");
+    var rows = Array.from(table.childNodes);
+    rows.slice(1).forEach(function (elm) {
+        if (elm.cells && elm.cells[1]) {
+            if (elm.cells[1].innerText.includes(value)) {
+                elm.style.visibility = "visible";
+            }
+            else {
+                elm.style.visibility = "hidden";
+            }
+        }
+    });
 };
