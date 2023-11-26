f529776bc75954498ec7b8fe1e6d8550ebc6c6d6
David <davite3@gmail.com>
2023-11-26 22:43:57 +0000

    prettier formatting on previous labs

--- lab1/problem-2/script.ts
+++ lab1/problem-2/script.ts
@@ -1,36 +1,37 @@
 const add_note = () => {
-    const data = <HTMLInputElement> document.getElementById("input_note")
-    const text = document.createTextNode(data.value)
+  const data = <HTMLInputElement>document.getElementById("input_note");
+  const text = document.createTextNode(data.value);
 
-    const notes = document.getElementById("notes")
-    const note = document.createElement("li")
-    
-    const colourSelect = <HTMLSelectElement> document.getElementById("colours")
+  const notes = document.getElementById("notes");
+  const note = document.createElement("li");
 
-    note.style.backgroundColor = colourSelect.options[colourSelect.selectedIndex].value;
-    note.textContent = data.value
-    
-    const deleteButton = <HTMLInputElement> document.createElement("input")
-    deleteButton.type = "button"
-    deleteButton.value = "Delete"
+  const colourSelect = <HTMLSelectElement>document.getElementById("colours");
 
-    deleteButton.onclick = () => deleteButton.parentElement.remove()
+  note.style.backgroundColor =
+    colourSelect.options[colourSelect.selectedIndex].value;
+  note.textContent = data.value;
 
-    const editButton = <HTMLInputElement> document.createElement("input")
-    editButton.type = "button"
-    editButton.value = "Edit"
+  const deleteButton = <HTMLInputElement>document.createElement("input");
+  deleteButton.type = "button";
+  deleteButton.value = "Delete";
 
-    editButton.onclick = () => {
-        let newString = prompt("Please edit your message", text.data)
-        
-        const newText = document.createTextNode(newString)
-        note.textContent = newString
-        // unsure why but writing to textContent will delete the child elements.
-        note.appendChild(editButton)
-        note.appendChild(deleteButton)
-    }
-    
-    note.appendChild(editButton)
-    note.appendChild(deleteButton)
-    notes.appendChild(note)
-}
+  deleteButton.onclick = () => deleteButton.parentElement.remove();
+
+  const editButton = <HTMLInputElement>document.createElement("input");
+  editButton.type = "button";
+  editButton.value = "Edit";
+
+  editButton.onclick = () => {
+    let newString = prompt("Please edit your message", text.data);
+
+    const newText = document.createTextNode(newString);
+    note.textContent = newString;
+    // unsure why but writing to textContent will delete the child elements.
+    note.appendChild(editButton);
+    note.appendChild(deleteButton);
+  };
+
+  note.appendChild(editButton);
+  note.appendChild(deleteButton);
+  notes.appendChild(note);
+};
--- lab1/problem-3/Content.ts
+++ lab1/problem-3/Content.ts
@@ -2,56 +2,53 @@
 // and every capital I with alowercase L
 // add funny images to let the user know they have been pranked!
 const main = () => {
+  const scheming: string[] = [
+    "https://st4.depositphotos.com/1001911/21888/v/1600/depositphotos_218880448-stock-illustration-scheming-emoticon.jpg",
+    "https://i.kym-cdn.com/entries/icons/original/000/031/260/Screen_Shot_2019-09-24_at_4.22.16_PM.png",
+    "https://media.tenor.com/IQcYOoTEuXYAAAAC/holy-moly.gif",
+    "https://media.tenor.com/PJCNWkE311YAAAAd/slowly-turning-emoji.gif",
+  ];
+  const imgs = document.getElementsByTagName("img");
 
-const scheming :string[] = [
-                   "https://st4.depositphotos.com/1001911/21888/v/1600/depositphotos_218880448-stock-illustration-scheming-emoticon.jpg"
-                   ,"https://i.kym-cdn.com/entries/icons/original/000/031/260/Screen_Shot_2019-09-24_at_4.22.16_PM.png"
-                   ,"https://media.tenor.com/IQcYOoTEuXYAAAAC/holy-moly.gif"
-                   ,"https://media.tenor.com/PJCNWkE311YAAAAd/slowly-turning-emoji.gif"
-               ]
-    const imgs = document.getElementsByTagName("img")
+  for (let i: number = 0; i < imgs.length; i++) {
+    imgs[i].src = scheming[Math.floor(Math.random() * scheming.length)];
+  }
 
-    for (let i :number = 0; i < imgs.length; i++) {
-        imgs[i].src = scheming[Math.floor(Math.random()*scheming.length)]
+  const p = document.getElementsByTagName("p");
+  const lis = document.getElementsByTagName("li");
+  const h1s = document.getElementsByTagName("h1");
+
+  const prank = (ps) => {
+    for (let i: number = 0; i < ps.length; i++) {
+      const par = ps[i].innerText.split("");
+      for (let ii = 0; ii < par.length; ii++) {
+        if (par[ii] === "l") par[ii] = "I";
+        else if (par[ii] === "I") par[ii] = "l";
+      }
+      ps[i].innerText = par.join("");
+      ps[i].style.color =
+        "#" +
+        Math.floor(Math.random() * 255).toString(16) +
+        Math.floor(Math.random() * 255).toString(16) +
+        Math.floor(Math.random() * 255).toString(16);
     }
+  };
 
-    const p = document.getElementsByTagName("p");
-    const lis = document.getElementsByTagName("li");
-    const h1s = document.getElementsByTagName("h1")
+  prank(p);
+  prank(lis);
+  prank(h1s);
 
-    const prank = (ps) => {
-        for (let i :number = 0; i < ps.length; i++) {
-            const par = ps[i].innerText.split('');
-            for(let ii = 0; ii <  par.length; ii++) {
-                if (par[ii] === "l")
-                    par[ii] = "I";
-                else if (par[ii] === "I")
-                    par[ii] = "l";
-            }
-            ps[i].innerText = par.join('');
-            ps[i].style.color = (
-                "#" + Math.floor(Math.random() * 255).toString(16)  +
-                Math.floor(Math.random() * 255).toString(16)  +
-                Math.floor(Math.random() * 255).toString(16)
-            )
-        }
+  const goof = (hs) => {
+    for (let i: number = 0; i < hs.length; i++) {
+      hs[i].innerText = "you just got pranked!";
     }
+  };
 
-    prank(p)
-    prank(lis)
-    prank(h1s)
-
-    const goof = (hs) => {
-        for (let i :number = 0; i < hs.length; i++) {
-            hs[i].innerText = "you just got pranked!"
-        }
-    }
-
-    goof(h1s)
-    goof(document.getElementsByTagName("h2"))
-    goof(document.getElementsByTagName("h3"))
-    goof(document.getElementsByTagName("h4"))
-    goof(document.getElementsByTagName("h5"))
-    goof(document.getElementsByTagName("h6"))
-}
-main()
+  goof(h1s);
+  goof(document.getElementsByTagName("h2"));
+  goof(document.getElementsByTagName("h3"));
+  goof(document.getElementsByTagName("h4"));
+  goof(document.getElementsByTagName("h5"));
+  goof(document.getElementsByTagName("h6"));
+};
+main();
