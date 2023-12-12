cfbe0425d388eced1539d79f62230f3153054dc2
David <davite3@gmail.com>
2023-12-12 22:27:53 +0000

    todolist implemented

--- lab4/src/App.tsx
+++ lab4/src/App.tsx
@@ -1,42 +1,64 @@
 import React from "react";
-import "./App.css";
 import Note from "./Note";
 
 function App() {
-  const [data, setData] = React.useState<string[]>([]);
+  const colours = [
+    "#94a3b8",
+    "#f87171",
+    "#fbbf24",
+    "#4ade80",
+    "#38bdf8",
+    "#818cf8",
+    "#c084fc",
+    "#fb7185",
+  ];
   const [inputText, setInputText] = React.useState<string>("");
 
+  const [data, setData] = React.useState<{ text: string; colour: string }[]>([
+    { text: "this", colour: colours[0] },
+    { text: "is", colour: colours[1] },
+    { text: "some", colour: colours[2] },
+    { text: "test", colour: colours[3] },
+    { text: "data", colour: colours[4] },
+  ]);
+  const [colour, setColour] = React.useState<string>(colours[0]);
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
   };
 
   const userin: any = <input type="text" onChange={handleChange} />;
   return (
-    <div
-      className="w-full h-full
-    flex 
-    items-center justify-center 
-    bg-black"
-    >
-      <>
-        <h1 className="text-white text-5xl font-bold top-0">To-Do App</h1>
-        {userin}
-        <button
-          className="bg-green-100"
-          onClick={() => {
-            setData(data.concat(inputText));
-          }}
-        >
-          Submit
-        </button>
-        <ul>
-          {data.map((x) => (
-            <li>
-              <Note data={x} />
-            </li>
-          ))}
-        </ul>
-      </>
+    <div className="bg-green-100 h-full">
+      <h1 className="text-3xl">To-Do App</h1>
+      {userin}
+      <button
+        className="rounded"
+        style={{background: colour}}
+        onClick={() => {
+          if (inputText.length !== 0) {
+            setData(data.concat({text:inputText,colour: colour}))
+          }
+        }}
+      >
+        Submit
+      </button>
+      <div className="flex">
+        {colours.map((x, i) => (
+          <div
+            className={"w-10 h-10"}
+            style={{ background: x }}
+            onClick={(e: any) => {
+                setColour(colours[i])
+            }}
+          ></div>
+        ))}
+      </div>
+
+      <div>
+        {data.map((_, id) => (
+          <Note data={{ id, data, setData }} />
+        ))}
+      </div>
     </div>
   );
 }
--- lab4/src/Note.tsx
+++ lab4/src/Note.tsx
@@ -1,14 +1,55 @@
 import React from "react";
 
-export default function Note(props: any) {
-  const [data, setData] = React.useState<string>(props.data);
+export default function Note(props: {
+  data: {
+    id: number;
+    data: {text:string, colour:string}[];
+    setData: React.Dispatch<React.SetStateAction<{text:string,colour:string}[]>>;
+  };
+}) {
+  const properties = props.data;
+
   return (
-    <>
-      <div className="flex items-center justify-center bg-yellow-500 w-auto rounded">
-        <p className="text-white font-bold text-l">{data}</p>
-        <button>Edit</button>
-        <button>Delete</button>
+    <div
+      className=" border-2 border-black 
+      rounded-lg
+      w-auto"
+      style={{background: properties.data[properties.id].colour}}
+    >
+      <span className="text-black">{properties.data[properties.id].text}</span>
+      <div>
+        <button
+          className="bg-white rounded-lg px-8"
+          onClick={() => {
+            const newText = prompt(
+              "Edit your text",
+              properties.data[properties.id].text,
+            );
+            newText !== "" && newText !== null
+              ? properties.setData(
+                  properties.data.map((x, i) => {
+                    if (i === properties.id) return {text: newText, colour:x.colour};
+                    return x;
+                  }),
+                )
+              : console.log("no text entered");
+          }}
+        >
+          Edit
+        </button>
+        <button
+          className="bg-white rounded-lg px-8"
+          onClick={() => {
+            const t = properties.data.filter((_, i) => i !== properties.id);
+            console.log(t);
+            properties.setData(
+              properties.data.filter((_, i) => i !== properties.id),
+            );
+          }}
+        >
+          Delete
+        </button>
       </div>
-    </>
+    </div>
   );
 }
